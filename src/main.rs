a6840b7323997c2019b5c8a3271bce27a2affc97
Tim McNamara <code@timmcnamara.co.nz>
2021-10-15 10:58:02 +1300

    challenge 2 beginning

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,67 @@
-fn median(a: Vec<f32>) -> Option<f32> {
+fn unique(a: Vec<f32>) -> Vec<f32> {
     todo!();
 }
 
-fn main() {
-    let answer = median(vec![1.0, 2.0, 5.0]);
+// advanced 1: use generic types
+// fn unique(a: Vec<T>) -> Vec<T> {
+//     todo!();
+// }
 
-    println!("median([1,2,5]) = {:?}", answer);
+// advanced 2: keep items in order
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//     todo!();
+// }
+
+// advanced 3: use iterators
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//     todo!();
+// }
+
+fn main() {
+    let input = vec![2.0, 1.0, 1.0];
+    let answer = unique(input);
+    println!("unique items -> {:?}", answer)
 }
+
 
 #[test]
 fn empty_list() {
     let input = vec![];
-    let expected_output = None;
-    let actual_output = median(input);
+    let expected_output = vec![];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
 
 #[test]
 fn sorted_list() {
     let input = vec![1.0, 4.0, 5.0];
-    let expected_output = Some(4.0);
-    let actual_output = median(input);
-    assert_eq!(actual_output, expected_output);
-}
-
-#[test]
-fn even_length() {
-    let input = vec![1.0, 3.0, 5.0, 6.0];
-    let expected_output = Some(4.0);
-    let actual_output = median(input);
+    let expected_output = vec![1.0, 4.0, 5.0];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
 
 #[test]
 fn unsorted_list() {
     let input = vec![1.0, 5.0, 2.0];
-    let expected_output = Some(2.0);
-    let actual_output = median(input);
+    let expected_output = vec![1.0, 5.0, 2.0];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+
+#[test]
+fn unsorted_list_with_duplicates() {
+    let input = vec![1.0, 5.0, 2.0, 2.0, 1.0];
+    let expected_output = vec![1.0, 5.0, 2.0];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list_with_duplicates() {
+    let mut input = vec![1.0, 5.0, 2.0, 2.0, 1.0];
+    input.sort_by(|x,y| x.partial_cmp(y).unwrap());
+    let expected_output = vec![1.0, 2.0, 5.0];
+    let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
