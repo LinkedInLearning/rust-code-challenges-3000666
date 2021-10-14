a68294421b9d3e16e27e978bebc1b3445d22eaa8
Tim McNamara <code@timmcnamara.co.nz>
2021-10-14 21:07:42 +1300

    Improve formatting

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 fn median(mut a: Vec<f32>) -> Option<f32> {
-  if a.is_empty() {
-    return None;
-  }
+    if a.is_empty() {
+        return None;
+    }
 
-  a.sort_by(|x,y| x.partial_cmp(y).unwrap());
- 
-  let n_elements = a.len();
-  let middle = n_elements / 2;
- 
-  let med = if n_elements % 2 == 0 {
-    (a[middle] + a[middle - 1]) / 2.0
-  } else {
-    a[middle]
-  };
- 
-  Some(med)
+    a.sort_by(|x, y| x.partial_cmp(y).unwrap());
+
+    let n_elements = a.len();
+    let middle = n_elements / 2;
+
+    let med = if n_elements % 2 == 0 {
+        (a[middle] + a[middle - 1]) / 2.0
+    } else {
+        a[middle]
+    };
+
+    Some(med)
 }
-
 
 fn main() {
     let answer = median(vec![1.0, 2.0, 5.0]);
@@ -24,21 +23,33 @@ fn main() {
     println!("median([1,2,5]) = {:?}", answer);
 }
 
-
-
-
 #[test]
-fn empty_input() {
+fn empty_list() {
     let input = vec![];
     let expected_output = None;
     let actual_output = median(input);
     assert_eq!(actual_output, expected_output);
 }
 
+#[test]
+fn sorted_list() {
+    let input = vec![1.0, 4.0, 5.0];
+    let expected_output = Some(4.0);
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
 
 #[test]
-fn one_two_five() {
-    let input = vec![1.0, 2.0, 5.0];
+fn even_length() {
+    let input = vec![1.0, 3.0, 5.0, 6.0];
+    let expected_output = Some(4.0);
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list() {
+    let input = vec![1.0, 5.0, 2.0];
     let expected_output = Some(2.0);
     let actual_output = median(input);
     assert_eq!(actual_output, expected_output);
