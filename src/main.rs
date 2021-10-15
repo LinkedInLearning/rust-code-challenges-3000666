3e43d8d57024dcc65c8f46bdf910975cddf0d4d4
Tim McNamara <code@timmcnamara.co.nz>
2021-10-15 21:28:02 +1300

    challenge 5 end

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,102 @@
-fn sort_usernames<T: AsRef<str>>(usernames: &mut Vec<T>) {
-    todo!();
+#[derive(Debug, PartialEq)]
+enum Pulse {
+    Short,
+    Long,
+}
+
+/// Represents a single character
+type Letter = Vec<Pulse>;
+
+/// Represents a string of characters
+type Message = Vec<Letter>;
+
+trait MorseCode {
+    fn to_morse_code(&self) -> Message;
+}
+
+impl MorseCode for String {
+    fn to_morse_code(&self) -> Message {
+        use Pulse::*;
+
+        let mut msg = Vec::with_capacity(self.len());
+        for c in self.chars() {
+            let letter = match c {
+                'A' | 'a' => vec![Short, Long],
+                'B' | 'b' => vec![Long, Short, Short, Short],
+                'C' | 'c' => vec![Long, Short, Long, Short],
+                'D' | 'd' => vec![Long, Short, Short],
+                'E' | 'e' => vec![Short],
+                'F' | 'f' => vec![Short, Short, Long, Short],
+                'G' | 'g' => vec![Long, Long, Short],
+                'H' | 'h' => vec![Short, Short, Short, Short, Short],
+                'I' | 'i' => vec![Short, Short],
+                'J' | 'j' => vec![Short, Long, Long, Long],
+                'K' | 'k' => vec![Long, Short, Long],
+                'L' | 'l' => vec![Short, Long, Short, Short],
+                'M' | 'm' => vec![Long, Long],
+                'N' | 'n' => vec![Long, Short],
+                'O' | 'o' => vec![Long, Long, Long],
+                'P' | 'p' => vec![Short, Long, Long, Short],
+                'Q' | 'q' => vec![Long, Long, Short, Long],
+                'R' | 'r' => vec![Short, Long, Short],
+                'S' | 's' => vec![Short, Short, Short],
+                'T' | 't' => vec![Long],
+                'U' | 'u' => vec![Short, Short, Long],
+                'V' | 'v' => vec![Short, Short, Short, Long],
+                'W' | 'w' => vec![Short, Long, Long],
+                'X' | 'x' => vec![Long, Short, Short, Long],
+                'Y' | 'y' => vec![Long, Short, Long, Long],
+                'Z' | 'z' => vec![Long, Long, Short, Short],
+
+                '1' => vec![Short, Long, Long, Long, Long],
+                '2' => vec![Short, Short, Long, Long, Long],
+                '3' => vec![Short, Short, Short, Long, Long],
+                '4' => vec![Short, Short, Short, Short, Long],
+                '5' => vec![Short, Short, Short, Short, Short],
+                '6' => vec![Long, Short, Short, Short, Short],
+                '7' => vec![Long, Long, Short, Short, Short],
+                '8' => vec![Long, Long, Long, Short, Short],
+                '9' => vec![Long, Long, Long, Long, Short],
+                '0' => vec![Long, Long, Long, Long, Long],
+                _ => continue,
+            };
+
+            msg.push(letter);
+        }
+        msg
+    }
 }
 
 fn main() {
-    let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
-
-    println!("unsorted: {:?}", &users);
-    sort_usernames(&mut users);
-    println!("sorted:   {:?}", &users);
+    let greeting = "Hello, world".to_string().to_morse_code();
+    println!("{:?}", greeting)
 }
 
 #[test]
-fn five_users() {
-    let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
-    let sorted = vec!["alison", "Amy", "Jennifer", "mike99", "Todd"];
-    sort_usernames(&mut users);
+fn hello_world() {
+    use Pulse::*;
 
-    assert_eq!(users, sorted);
+    let expected = vec![
+        vec![Short, Short, Short, Short, Short],
+        vec![Short],
+        vec![Short, Long, Short, Short],
+        vec![Short, Long, Short, Short],
+        vec![Long, Long, Long],
+        vec![Short, Long, Long],
+        vec![Long, Long, Long],
+        vec![Short, Long, Short],
+        vec![Short, Long, Short, Short],
+        vec![Long, Short, Short],
+    ];
+
+    let actual = "Hello, world".to_string().to_morse_code();
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn whole_alphabet() {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz1234567890".to_string();
+
+    alphabet.to_morse_code();
+    alphabet.to_uppercase().to_morse_code();
 }
