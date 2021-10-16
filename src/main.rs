32693dcca53a89eac216bafbcad5803ae1fe060d
Tim McNamara <code@timmcnamara.co.nz>
2021-10-16 16:47:02 +1300

    challenge 6 end

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,7 @@ struct Hand {
 
 impl Hand {
     fn new() -> Self {
-        Hand {
-            cards: vec![],
-        }
+        Hand { cards: vec![] }
     }
 
     fn add(&mut self, card: Card) {
@@ -32,8 +30,37 @@ impl Hand {
     }
 
     fn value(&self) -> usize {
-        // TODO: implement this method
-        0 
+        use Card::*;
+
+        let mut subtotal = 0;
+        let mut aces_seen = 0;
+
+        for card in self.cards.iter() {
+            subtotal += match card {
+                Ace => {
+                    aces_seen += 1;
+                    0
+                },
+                Two => 2,
+                Three => 3,
+                Four => 4,
+                Five => 5,
+                Six => 6,
+                Seven => 7,
+                Eight => 8,
+                Nine => 9,
+                Jack => 10,
+                Queen => 10,
+                King => 10,
+            };
+        }
+
+        for _ in 0..aces_seen {
+            let ace_value = if subtotal <= 10 { 11 } else { 1 };
+            subtotal += ace_value;
+        }
+
+        subtotal
     }
 
     fn is_loosing_hand(&self) -> bool {
@@ -46,7 +73,6 @@ fn main() {
     hand.add(Card::King);
     hand.add(Card::Ace);
 }
-
 
 #[test]
 fn empty_hand() {
@@ -70,7 +96,7 @@ fn risky_hand() {
     hand.add(Card::King);
     hand.add(Card::Queen);
     hand.add(Card::Ace);
-    
+
     assert_eq!(hand.value(), 21);
 }
 
@@ -80,7 +106,7 @@ fn oops() {
     hand.add(Card::King);
     hand.add(Card::Seven);
     hand.add(Card::Five);
-    
+
     assert!(hand.is_loosing_hand());
     assert_eq!(hand.value(), 22);
 }
