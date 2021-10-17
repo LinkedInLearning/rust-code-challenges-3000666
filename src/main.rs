90489bad4cac4725cb8e1c645759c1749f5998a3
Tim McNamara <code@timmcnamara.co.nz>
2021-10-17 20:22:45 +1300

    chapter 8 end

--- src/main.rs
+++ src/main.rs
@@ -18,32 +18,50 @@ impl Temperature {
         }
     }
 
-    fn as_celsius(&self) -> f32 {
-        todo!();
+    fn to_celsius(&self) -> f32 {
+        match self.scale {
+            Scale::Celsius => self.degrees,
+            Scale::Fahrenheit => (self.degrees - 32.0) * (5.0 / 9.0),
+        }
     }
 
-    fn as_fahrenheit(&self) -> f32 {
-        todo!();
+    fn to_fahrenheit(&self) -> f32 {
+        match self.scale {
+            Scale::Celsius => ((9.0 / 5.0) * self.degrees) + 32.0,
+            Scale::Fahrenheit => self.degrees,
+        }
     }
 }
 
 fn main() {
     let temp = Temperature::new(20.0);
 
-    println!("{}C is {}F", temp.as_celsius(), temp.as_fahrenheit());
+    println!("fun fact: 20°C is an integer in celsius and fahrenheit");
+    println!("          {:.1}°C = {:.1}°F", temp.to_celsius(), temp.to_fahrenheit());
 }
 
 #[test]
 fn one_degree() {
     let cold = Temperature::new(1.0);
-    assert!((cold.as_fahrenheit() - 33.8) < 0.01);
-    assert!((cold.as_fahrenheit() - 33.8) >= 0.0);
+    assert!((cold.to_fahrenheit() - 33.8) < 0.01);
+    assert!((cold.to_fahrenheit() - 33.8) >= 0.0);
 }
 
 #[test]
 fn boiling() {
     let hot = Temperature::new(100.0);
-    assert!((hot.as_fahrenheit() - 212.0) < 0.01);
-    assert!((hot.as_fahrenheit() - 212.0) >= 0.0);
+    assert!((hot.to_fahrenheit() - 212.0) < 0.01);
+    assert!((hot.to_fahrenheit() - 212.0) >= 0.0);
+}
+
+#[test]
+fn freezing() {
+    let freezing = Temperature {
+        degrees: Temperature::new(0.0).to_fahrenheit(),
+        scale: Scale::Fahrenheit,
+    };
+
+    assert!(freezing.to_celsius() < 0.001);
+    assert!(freezing.to_celsius() > -0.01);
 }
 
