455c9a34aff4ab125bd6ee09bc7b105d1d03541f
Tim McNamara <code@timmcnamara.co.nz>
2021-10-18 17:49:11 +1300

    challenge 13 begin

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,10 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-enum ParseColorError {
-    RedOutOfBounds,
-    BlueOutOfBounds,
-    GreenOutOfBounds,
-    Invalid,
-}
-
-#[derive(Debug, PartialEq)]
-struct Rgb {
-    r: u8,
-    g: u8,
-    b: u8,
-}
+struct Rgb; // TODO: design data structure
 
 impl FromStr for Rgb {
-    type Err = ParseColorError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if let Some(hex) = s.strip_prefix('#') {
-            let r = u8::from_str_radix(&hex[0..2], 16)
-                .or_else(|_| Err(ParseColorError::RedOutOfBounds))?;
-            let g = u8::from_str_radix(&hex[2..4], 16)
-                .or_else(|_| Err(ParseColorError::BlueOutOfBounds))?;
-            let b = u8::from_str_radix(&hex[4..6], 16)
-                .or_else(|_| Err(ParseColorError::GreenOutOfBounds))?;
-
-            Ok(Rgb { r, g, b })
-        } else {
-            // no leading #
-            Err(ParseColorError::Invalid)
-        }
-    }
+    // TODO: implement trait
 }
 
 impl Display for Rgb {
