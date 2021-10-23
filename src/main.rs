beb2af9a05aa8ace6e3d08129ee71eb6ba27eef3
Tim McNamara <code@timmcnamara.co.nz>
2021-10-23 21:29:20 +1300

    Add printing

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,31 @@ trait MorseCode {
     fn to_morse_code(&self) -> Message;
 }
 
+impl std::fmt::Display for Pulse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Pulse::Short => write!(f, "."),
+            Pulse::Long => write!(f, "_"),
+        }
+    }
+}
+
+fn print_morse_code(code: &Message) {
+    for letter in code.iter() {
+        for pulse in letter.iter() {
+            print!("{}", pulse);
+        };
+        print!(" ");
+    };
+    println!();
+}
+
 fn main() {
-    let greeting = "Hello, world".to_string().to_morse_code();
-    println!("{:?}", greeting)
+    let greeting = "Hello, world"
+        .to_string()
+        .to_morse_code();
+    
+    print_morse_code(greeting);
 }
 
 #[test]
