cc3b512ed40edc2724534d15a67be00dc7f4a642
Tim McNamara <code@timmcnamara.co.nz>
2021-10-24 10:46:19 +1300

    fix test

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn hello_world() {
     use Pulse::*;
 
     let expected = vec![
-        vec![Short, Short, Short, Short, Short],
+        vec![Short, Short, Short, Short],
         vec![Short],
         vec![Short, Long, Short, Short],
         vec![Short, Long, Short, Short],
