4837f80472613d096eb53126d18741c783e2279b
Tim McNamara <code@timmcnamara.co.nz>
2021-10-25 14:01:06 +1300

    rename sum() to sum_with_missing()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn sum(numbers: Vec<Option<i32>>) -> i32 {
+fn sum_with_missing(numbers: Vec<Option<i32>>) -> i32 {
     todo!();
 }
 
@@ -10,23 +10,23 @@ fn main() {
 #[test]
 fn empty() {
     let nn = vec![];
-    assert_eq!(sum(nn), 0);
+    assert_eq!(sum_with_missing(nn), 0);
 }
 
 #[test]
 fn no_missing() {
     let nn = vec![Some(1), Some(5), Some(4)];
-    assert_eq!(sum(nn), 10);
+    assert_eq!(sum_with_missing(nn), 10);
 }
 
 #[test]
 fn some_missing() {
     let nn = vec![None, Some(1), Some(5), Some(4), None, None];
-    assert_eq!(sum(nn), 10);
+    assert_eq!(sum_with_missing(nn), 10);
 }
 
 #[test]
 fn all_missing() {
     let nn = vec![None, None, None];
-    assert_eq!(sum(nn), 0);
+    assert_eq!(sum_with_missing(nn), 0);
 }
