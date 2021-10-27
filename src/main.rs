5ffa535f48aa9a23ec1040a3167323e9492d33d2
Tim McNamara <code@timmcnamara.co.nz>
2021-10-27 21:55:38 +1300

    add extra test

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,14 @@ fn round_trip() {
     use run_length_encoding::*;
 
     let input = "LinkedIn";
+    println!("{}", encode(input));
     assert_eq!(decode(&encode(input)), input);
 }
 
+#[test]
+fn long_run() {
+    use run_length_encoding::*;
+
+    let input = "AAAAA AAAAAAAAAA AAAAAAAAAAAAAAAAAAAA";
+    assert_eq!(encode(input), "5A1 9A1A1 9A9A2A");
+}
