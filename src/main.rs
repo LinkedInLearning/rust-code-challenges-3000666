b0b1612eeacdf9741b99fa3c4e0af738aac67bdb
Tim McNamara <code@timmcnamara.co.nz>
2021-11-01 20:57:20 +1300

    begin challenge

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
 mod vigenere {
-    const ALPHABET: [u8; 26] = *b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const A: u8 = b'A';
-    const Z: u8 = b'Z';
-    const WRAP: u8 = 26; // ALPHABET.len() as u8
-
-    fn clean_input(input: &str) -> impl Iterator<Item = u8> + '_ {
-        input.bytes().filter_map(|x| match x {
-            A..=Z => Some(x),
-            b'a'..=b'z' => Some(x - (b'a' - A)),
-            _ => None,
-        })
-    }
-
     pub fn encrypt(plaintext: &str, key: &str) -> String {
-        let mut key_iter = key.bytes().map(|k| k - A).cycle();
-
-        let encrypted = clean_input(plaintext)
-            .map(|x| {
-                let offset = key_iter.next().unwrap();
-                ((x - A) + offset) % WRAP + A
-            })
-            .collect();
-
-        String::from_utf8(encrypted).unwrap()
+        String::new() // Optional
     }
 
     pub fn decrypt(plaintext: &str, key: &str) -> String {
-        let mut key_iter = key.bytes().map(|k| k - b'A').cycle();
-
-        let tmp = clean_input(plaintext)
-            .map(|x| {
-                let offset = key_iter.next().unwrap();
-                ((x + WRAP - A) - offset) % WRAP + A
-            })
-            .collect();
-
-        String::from_utf8(tmp).unwrap()
+        todo!()
     }
 }
 
@@ -44,7 +13,7 @@ fn main() {
     let ciphertext = "
     PVCDJG
     PAYCMY
-    JRKUC
+    JR KUC
     ";
     let plaintext = vigenere::decrypt(&ciphertext, key);
 
